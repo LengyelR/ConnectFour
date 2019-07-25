23deac6684dcc5b83262775705a352a6a7bb912f
surface-rich <surface-rich_email>
2019-07-25 20:45:19 +0100

    do not reuse/share the layers

--- network.py
+++ network.py
@@ -6,57 +6,46 @@ INPUT_SHAPE = (2, 6, 7)
 
 
 class ConvolutionalBlock:
-    def __init__(self, reg):
-        self.conv = tf.keras.layers.Conv2D(64, (3, 3), padding='same', kernel_regularizer=reg)
-        self.bn = tf.keras.layers.BatchNormalization()
-        self.relu = tf.keras.layers.Activation('relu')
+    def __init__(self, filters, shape, reg, name):
+        self.conv = tf.keras.layers.Conv2D(filters, shape,
+                                           padding='same',
+                                           kernel_regularizer=reg,
+                                           name=f'{name}_conv_block')
+        self.bn = tf.keras.layers.BatchNormalization(name=f'{name}_bn_block')
+        self.relu = tf.keras.layers.Activation('relu', name=f'{name}_relu_block')
 
     def __call__(self, x):
         x = self.conv(x)
         x = self.bn(x)
-        out = self.relu(x)
-        return out
+        return self.relu(x)
 
 
 class ResidualBlock:
-    def __init__(self, reg):
-        self.conv = tf.keras.layers.Conv2D(64, (3, 3), padding='same', kernel_regularizer=reg)
-        self.bn = tf.keras.layers.BatchNormalization()
-        self.relu = tf.keras.layers.Activation('relu')
-        # without wrapping the addition in a lambda layer, we cannot save the keras model...
-        # passing 2 params isn't possible, we must use a tuple...
-        self.add = tf.keras.layers.Lambda(lambda ab_tuple: ab_tuple[0] + ab_tuple[1])
+    def __init__(self, idx, reg):
+        self.conv1 = ConvolutionalBlock(64, (3, 3), reg, f'resi1_{idx}')
+        self.conv2 = ConvolutionalBlock(64, (3, 3), reg, f'resi2_{idx}')
+        self.relu = tf.keras.layers.Activation('relu', name=f'resi_relu_{idx}')
 
     def __call__(self, x):
         residual = x
 
-        out = self.conv(x)
-        out = self.bn(out)
-        out = self.relu(out)
+        out = self.conv1(x)
+        out = self.conv2(out)
 
-        out = self.conv(out)
-        out = self.bn(out)
-        out = self.relu(out)
-
-        out = self.add((out, residual))
+        out = tf.keras.layers.add([out, residual])
         out = self.relu(out)
         return out
 
 
 class PolicyHead:
     def __init__(self, reg):
+        self.conv = ConvolutionalBlock(16, (1, 1), reg, 'policy')
         self.flatten = tf.keras.layers.Flatten()
-        self.bn = tf.keras.layers.BatchNormalization()
-        self.conv = tf.keras.layers.Conv2D(2, (1, 1), padding='same', kernel_regularizer=reg)
         self.fc = tf.keras.layers.Dense(7, kernel_regularizer=reg)   # column size of the board (i.e. moves)
-        self.relu = tf.keras.layers.Activation('relu')
         self.softmax = tf.keras.layers.Activation('softmax', name="policy_head")
 
     def __call__(self, x):
         x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-
         x = self.flatten(x)
         x = self.fc(x)
         return self.softmax(x)
@@ -64,18 +53,15 @@ class PolicyHead:
 
 class ValueHead:
     def __init__(self, reg):
+        self.conv = ConvolutionalBlock(8, (1, 1), reg, 'value')
         self.flatten = tf.keras.layers.Flatten()
-        self.bn = tf.keras.layers.BatchNormalization()
-        self.relu = tf.keras.layers.Activation('relu')
         self.fc1 = tf.keras.layers.Dense(256, kernel_regularizer=reg)
+        self.relu = tf.keras.layers.Activation('relu', name='value_relu')
         self.fc2 = tf.keras.layers.Dense(1, kernel_regularizer=reg)
-        self.conv = tf.keras.layers.Conv2D(1, (1, 1), padding='same', kernel_regularizer=reg)
         self.tanh = tf.keras.layers.Activation('tanh', name='value_head')
 
     def __call__(self, x):
         x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
 
         x = self.flatten(x)
         x = self.fc1(x)
@@ -86,25 +72,25 @@ class ValueHead:
 
 
 class Con4Zero:
-    def __init__(self, input_shape, res_block):
+    def __init__(self, input_shape):
         l2 = tf.keras.regularizers.l2(0.01)
 
-        self.block = res_block(l2)
         self.policy_head = PolicyHead(l2)
         self.value_head = ValueHead(l2)
-        self.conv = ConvolutionalBlock(l2)
+        self.conv = ConvolutionalBlock(64, (3, 3), l2, 'first')
         self.input_shape = input_shape
 
-    def _residual_tower(self, x, n):
-        for _ in range(n):
-            x = self.block(x)
+    @staticmethod
+    def _residual_tower(x, reg=tf.keras.regularizers.l2(0.01),  n=20):
+        for i in range(n):
+            x = ResidualBlock(i, reg)(x)
         return x
 
     def __call__(self):
         board = tf.keras.layers.Input(shape=self.input_shape, name="board_state_input")
 
         x = self.conv(board)
-        x = self._residual_tower(x, 2)  # todo: higher tower
+        x = self._residual_tower(x)
         policy = self.policy_head(x)
         value = self.value_head(x)
 
@@ -115,21 +101,20 @@ class Con4Zero:
     def loss():
         """
         p, v = f_theta(s)
-        loss := (z - v)**2 - pi * log(p) + l2reg(theta)
+        loss := (z - v)**2 - pi * log(p)
         where
             p is the returned policy from the network
             v is the estimated outcome from the network
             z is the real outcome
             pi is MCTS outcome ("the improved p")
-            theta is the weight parameters of the network
 
-        :return: MSE + CROSS-ENTROPY + l2reg
+        :return: MSE + CROSS-ENTROPY
         """
 
         def _loss(y_true, y_pred):
             mse = tf.keras.losses.mean_squared_error(y_true[1], y_pred[1])
             ce = tf.keras.losses.categorical_crossentropy(y_true[0], y_pred[0])
-            return mse + ce  # todo: + K.square(weights)*c
+            return mse + ce
 
         return _loss
 
@@ -163,8 +148,14 @@ if __name__ == "__main__":
         pi[0] = 1.0
         return [np.tile(pi, (n, 1)), np.ones(n)]
 
-    creator = Con4Zero(INPUT_SHAPE, ResidualBlock)
+    creator = Con4Zero(INPUT_SHAPE)
     neural = creator()
+    print(neural.summary())
+
+    sess = tf.keras.backend.get_session()
+    writer = tf.summary.FileWriter("graph_output", sess.graph)
+    writer.close()
+
     run_model(neural)
 
     neural.compile(
@@ -182,5 +173,3 @@ if __name__ == "__main__":
         validation_data=(np.random.randn(*test_shape), create_data(TEST)),
         validation_freq=10
     )
-
-    run_model(neural)
