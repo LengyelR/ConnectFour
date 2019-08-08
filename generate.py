84c543fa46a1acb0c436bccc0563bd5c5472141b
lengyelr <10964681+LengyelR@users.noreply.github.com>
2019-08-09 00:25:39 +0100

    added ray for cluster computing

--- generate.py
+++ generate.py
@@ -2,75 +2,86 @@ import os
 import uuid
 import pickle
 import logging
+import ray
 
 import search
 import connect4
 import inference
 
-
 _logger = logging.getLogger(__name__)
 
 
-def _update_match(winner, game):
-    if winner == 0:
-        z = 0
-    else:
-        z = 1 if winner == 1 else -1
-
-    for step in game:
-        step.append(z)
-
-
-def self_play(gen, iteration, tau, folder='', batches=10, n=100):
+def main(gen, iteration, tau, folder='', batches=10, n=100):
     """
-    :param gen: folder name of the generation (e.g. gen-4)
-    :param iteration: execute this many mcts iterations
-    :param tau: mcts parameter, controlling exploration
-    :param folder: root folder (training/generation/guid/data will be created here)
-    :param batches: number of batches (each batch contains n games)
-    :param n: number of games to play in a batch
-    :return:
-    """
-
-    frozen_model_path = os.path.join(folder, 'model', gen, 'frozen_model.pb')
+     :param gen: folder name of the generation (e.g. gen-4)
+     :param iteration: execute this many mcts iterations
+     :param tau: mcts parameter, controlling exploration
+     :param folder: root folder (training/generation/guid/data will be created here)
+     :param batches: number of batches (each batch contains n games)
+     :param n: number of games to play in a batch
+     :return:
+     """
 
     guid = uuid.uuid4()
-    folder = os.path.join(folder, 'training', gen, str(guid))
-    os.makedirs(folder)
+    training_folder = os.path.join(folder, 'training', gen, str(guid))
+    os.makedirs(training_folder)
 
-    # here tree and model are shared between players
-    engine = connect4.GameEngine()
-    model = inference.FrozenModel(frozen_model_path)
-    mcts = search.Mcts(iteration, model, engine)
+    sp = SelfPlay.remote(gen, iteration, folder)
 
     for batch_no in range(batches):
-        self_play_data = []
-        _logger.debug(f'generating: {guid} -> {batch_no}')
-
+        game_ids = []
         for i in range(n):
-            s = engine.empty_board()
-            root = search.Node(0, 0, s, 2)  # first move is the child node of root, so root belongs to 2nd player
-            game_steps = []
+            game_id = sp.self_play.remote(gen, iteration, tau)
+            game_ids.append(game_id)
 
-            while True:
-                pi, best_node = mcts.search(root, tau)
-                game_steps.append([s, pi, best_node.Q, best_node.player])
-
-                if best_node.terminal is not None:
-                    break
-
-                root = best_node
-
-            winner = best_node.player if best_node.terminal == 1 else 0
-            _update_match(winner, game_steps)
-
-            _logger.debug(f'game{i} has been finished. Winner is {winner}')
-            self_play_data.extend(game_steps)
-        _logger.debug(f'saving: {guid} -> {batch_no}')
+        batch = ray.get(game_ids)
+        flattened = [step for game in batch for step in game]
         data_name = f'{batch_no}_{n}_self_play.pkl'
-        full_path = os.path.join(folder, data_name)
+        full_path = os.path.join(training_folder, data_name)
         with open(full_path, 'wb') as f:
-            pickle.dump(self_play_data, f, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(flattened, f, pickle.HIGHEST_PROTOCOL)
+
+
+@ray.remote
+class SelfPlay:
+    def __init__(self, gen, iteration, folder=''):
+        self.gen = gen
+        self.iteration = iteration
+        frozen_model_path = os.path.join(folder, 'model', gen, 'frozen_model.pb')
+
+        # here tree and model are shared between players
+        self.engine = connect4.GameEngine()
+        self.model = inference.FrozenModel(frozen_model_path)
+        self.mcts = search.Mcts(iteration, self.model, self.engine)
+
+    def self_play(self, tau):
+        s = self.engine.empty_board()
+        root = search.Node(0, 0, s, 2)  # first move is the child node of root, so root belongs to 2nd player
+        game_steps = []
+
+        while True:
+            pi, best_node = self.mcts.search(root, tau)
+            game_steps.append([s, pi, best_node.Q, best_node.player])
+
+            if best_node.terminal is not None:
+                break
+
+            root = best_node
+
+        winner = best_node.player if best_node.terminal == 1 else 0
+        self._update_match(winner, game_steps)
+
+        return game_steps
+
+    @staticmethod
+    def _update_match(winner, game):
+        if winner == 0:
+            z = 0
+        else:
+            z = 1 if winner == 1 else -1
+
+        for step in game:
+            step.append(z)
 
 
 if __name__ == '__main__':
@@ -113,6 +124,12 @@ if __name__ == '__main__':
         default=1.0,
         help='Temperature parameter for the exponentiated visit counts.'
     )
+    parser.add_argument(
+        '--redis_address', '-r',
+        type=str,
+        default='127.0.0.1:5000',
+        help='Head node\'s redis address'
+    )
 
     flags, _ = parser.parse_known_args()
 
@@ -123,9 +140,11 @@ if __name__ == '__main__':
     _logger.setLevel(logging.DEBUG)
     _logger.addHandler(fh)
 
-    self_play(flags.generation,
-              flags.iter,
-              flags.tau,
-              flags.folder,
-              flags.batches,
-              flags.batch_size)
+    ray.init(redis_address=flags.redis_address)
+
+    main(flags.generation,
+         flags.iter,
+         flags.tau,
+         flags.folder,
+         flags.batches,
+         flags.batch_size)
