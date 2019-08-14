58d6166b4263b0f96346b72b08f1ad995457199a
lengyelr <10964681+LengyelR@users.noreply.github.com>
2019-08-14 22:57:11 +0100

    log progress during data generation

--- generate.py
+++ generate.py
@@ -12,53 +12,62 @@ import inference
 _logger = logging.getLogger(__name__)
 
 
-def main(gen, iteration, tau, folder='', batches=10, n=100, num_workers=100):
-    """
-     :param gen: folder name of the generation (e.g. gen-4)
-     :param iteration: execute this many mcts iterations
-     :param tau: mcts parameter, controlling exploration
-     :param folder: root folder (training/generation/guid/data will be created here)
-     :param batches: number of batches (each batch contains n games)
-     :param n: number of games to play in a batch
-     :param num_workers: number of workers to launch
-     :return:
-     """
-    guid = uuid.uuid4()
-    training_folder = os.path.join(folder, 'training', gen, str(guid))
-    os.makedirs(training_folder)
+class Process:
+    def __init__(self, gen, iteration, tau, folder='', num_batches=10, batch_size=100, num_workers=100):
+        """
+        :param gen: folder name of the generation (e.g. gen-4)
+        :param iteration: execute this many mcts iterations
+        :param tau: mcts parameter, controlling exploration
+        :param folder: root folder (training/generation/guid/data will be created here)
+        :param num_batches: number of batches (each batch contains n games)
+        :param batch_size: number of games to play in a batch
+        :param num_workers: number of workers to launch
+        :return:
+        """
+        self.gen = gen
+        self.iteration = iteration
+        self.tau = tau
+        self.folder = folder
+        self.num_batches = num_batches
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.workers = [SelfPlay.remote(gen, iteration, folder) for _ in range(num_workers)]
 
-    workers = [SelfPlay.remote(gen, iteration, folder) for _ in range(num_workers)]
+    def start(self):
+        guid = uuid.uuid4()
+        training_folder = os.path.join(self.folder, 'training', self.gen, str(guid))
+        os.makedirs(training_folder)
 
-    for batch_no in range(batches):
-        game_ids = start_workers(tau, workers, n)
-        wait(game_ids, 90, 10)
+        for batch_no in range(self.num_batches):
+            game_ids = self._start_workers()
+            self._show_progress(game_ids, 120.0)
 
-        batch = ray.get(game_ids)
-        flattened = [step for game in batch for step in game]
-        data_name = f'{batch_no}_{n}_self_play.pkl'
-        full_path = os.path.join(training_folder, data_name)
-        with open(full_path, 'wb') as f:
-            pickle.dump(flattened, f, pickle.HIGHEST_PROTOCOL)
+            batch = ray.get(game_ids)
+            flattened = [step for game in batch for step in game]
+            data_name = f'{batch_no}_{self.batch_size}_self_play.pkl'
+            full_path = os.path.join(training_folder, data_name)
 
+            _logger.debug(f'saving batch: {data_name}')
+            with open(full_path, 'wb') as f:
+                pickle.dump(flattened, f, pickle.HIGHEST_PROTOCOL)
 
-def start_workers(tau, workers, num_games):
-    i = 0
-    game_ids = []
-    while True:
-        for worker in workers:
-            if i == num_games:
-                return game_ids
-            game_ids.append(worker.self_play.remote(tau))
-            i += 1
+    def _start_workers(self):
+        i = 0
+        game_ids = []
+        while True:
+            for worker in self.workers:
+                if i == self.batch_size:
+                    return game_ids
+                game_ids.append(worker.self_play.remote(self.tau))
+                i += 1
 
+    def _show_progress(self, game_ids, freq):
+        i = 0
 
-def wait(object_ids, seconds, freq):
-    until = time.time() + seconds
-    remaining = 1
-    while time.time() < until and remaining > 0:
-        _, remaining_ids = ray.wait(object_ids, timeout=freq)
-        remaining = len(remaining_ids)
-        _logger.debug('remaining:', remaining)
+        while i < self.batch_size:
+            i += self.num_workers
+            ready_ids, remaining_ids = ray.wait(game_ids, num_returns=i, timeout=freq)
+            _logger.debug(f'remaining {len(remaining_ids)} / {len(game_ids)}')
 
 
 @ray.remote
@@ -173,14 +182,15 @@ if __name__ == '__main__':
 
     ray.init(redis_address=flags.redis_address)
     nodes = set(ray.get([get_node_ip.remote() for _ in range(1000)]))
-    print('\nNodes:', len(nodes), random.sample(nodes, min(5, flags.workers)))
-    print('CPU count:', ray.cluster_resources()['CPU'])
-    print()
+    _logger.debug(f'Nodes: {len(nodes)}, {random.sample(nodes, min(5, flags.workers))}')
+    _logger.debug(f'CPU count: {ray.cluster_resources()["CPU"]}')
 
-    main(flags.generation,
-         flags.iter,
-         flags.tau,
-         flags.folder,
-         flags.batches,
-         flags.batch_size,
-         flags.workers)
+    main = Process(
+            flags.generation,
+            flags.iter,
+            flags.tau,
+            flags.folder,
+            flags.batches,
+            flags.batch_size,
+            flags.workers)
+    main.start()
